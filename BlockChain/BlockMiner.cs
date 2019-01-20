ddfdc5247012ecf95449bb8f3e2debe9291a7133
John <johnd4390@gmail.com>
2019-01-20 17:04:43 -0500

    First commit

---
+++ BlockChain/BlockMiner.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlockChain
+{
+    public static class BlockMiner
+    {
+        public static void Mine(Block block, int zeroes)
+        {
+            block.Nonce = 0;
+
+            while (!HashHasStartingZeroes(block.Hash, zeroes))
+            {
+                block.Nonce++;
+                block.Hash = BlockHashCalculator.CalculateHash(block);
+            }
+        }
+
+        private static bool HashHasStartingZeroes(string hash, int startingZeroes)
+        {
+            return hash.Substring(0, startingZeroes).Equals(NumberOfZeroes(startingZeroes));
+        }
+
+        private static string NumberOfZeroes(int difficulty)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            for (var i = difficulty; i > 0; i--)
+            {
+                stringBuilder.Append('0');
+            }
+            return stringBuilder.ToString();
+        }
+
+        //public void MineBlock(int difficulty)
+        //{
+        //    var nonce = 0;
+
+        //    while (!getHash().substring(0, difficulty).equals(Utils.zeros(difficulty)))
+        //    {
+        //        nonce++;
+        //        hash = Block.calculateHash(this);
+        //    }
+        //}
+    }
+}
---
+++ BlockChain/Blockchain.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlockChain
+{
+    public class Blockchain
+    {
+        private int Difficulty;
+        private List<Block> Blocks;
+
+        public Blockchain(int difficulty)
+        {
+            Difficulty = difficulty;
+            Blocks = new List<Block>();
+            // create the first block
+            Block block = new Block(0, DateTime.Now.Ticks, null, "First Block");
+            BlockMiner.Mine(block, difficulty);
+            Blocks.Add(block);
+        }
+
+        public Block GetLatestBlock()
+        {
+            return Blocks[Blocks.Count - 1];
+        }
+
+        public Block NewBlock(String data)
+        {
+            Block latestBlock = GetLatestBlock();
+            return new Block(latestBlock.Index + 1, DateTime.Now.Ticks, latestBlock.Hash, data);
+        }
+
+        public void AddBlock(Block block)
+        {
+            if (block != null)
+            {
+                BlockMiner.Mine(block, Difficulty);
+                Blocks.Add(block);
+            }
+        }
+
+        public bool IsValid()
+        {
+            if (!Blocks.IsFirstBlockValid())
+            {
+                return false;
+            }
+
+            for (int i = 1; i < Blocks.Count; i++)
+            {
+                Block currentBlock = Blocks[i];
+                Block previousBlock = Blocks[i - 1];
+
+                if (!currentBlock.IsValidNewBlock(previousBlock))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder builder = new StringBuilder();
+
+            foreach (Block block in Blocks)
+            {
+                builder.Append(block).Append("\n");
+            }
+
+            return builder.ToString();
+        }
+    }
+}
