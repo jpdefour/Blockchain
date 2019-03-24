cb02f72f8fda83952bc3401ccaa432ce21e58c65
John <johnd4390@gmail.com>
2019-03-24 19:25:35 -0400

    Moved files to a class library for easy importing

---
+++ BlockChainLibrary/BlockHashCalculator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlockChain
+{
+    public static class BlockHashCalculator
+    {
+        public static String CalculateHash(Block block)
+        {
+            if (block?.CombinedString != null)
+            {
+                SHA256Managed crypto;
+
+                try
+                {
+                    crypto = new SHA256Managed();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    Console.WriteLine(e.InnerException);
+                    Console.ReadLine();
+                    return null;
+                }
+
+                var txt = block.CombinedString;
+                byte[] bytes = crypto.ComputeHash(Encoding.ASCII.GetBytes(txt));
+                StringBuilder builder = new StringBuilder();
+
+                foreach (byte b in bytes)
+                {
+                    var hex = (0xff & b).ToString("x");
+
+                    if (hex.Count() == 1)
+                    {
+                        builder.Append('0');
+                    }
+
+                    builder.Append(hex);
+                }
+
+                return builder.ToString();
+            }
+
+            return null;
+        }
+    }
+}
---
+++ BlockChainLibrary/BlockValidator.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlockChain
+{
+    public static class BlockValidator
+    {
+        public static bool IsFirstBlockValid(this List<Block> blocks)
+        {
+            Block firstBlock = blocks[0];
+
+            if (firstBlock.Index != 0)
+            {
+                return false;
+            }
+
+            if (firstBlock.PreviousHash != null)
+            {
+                return false;
+            }
+
+            if (IsBlockHashNotNullAndHashValid(firstBlock))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static bool IsValidNewBlock(this Block newBlock, Block previousBlock)
+        {
+            if (BlocksAreNotNull(newBlock, previousBlock))
+            {
+                if (previousBlock.Index + 1 != newBlock.Index)
+                {
+                    return false;
+                }
+
+                if (newBlock.PreviousHash == null ||
+                    !newBlock.PreviousHash.Equals(previousBlock.Hash))
+                {
+                    return false;
+                }
+
+                if (IsNewBlockPreviousHashNotNullAndPreviousHashValid(newBlock, previousBlock))
+                {
+                    return false;
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool IsBlockHashNotNullAndHashValid(Block block)
+        {
+            return block.Hash == null || !BlockHashCalculator.CalculateHash(block).Equals(block.Hash);
+        }
+
+        public static bool IsNewBlockPreviousHashNotNullAndPreviousHashValid(Block newBlock, Block previousBlock)
+        {
+            return newBlock.PreviousHash == null || !newBlock.PreviousHash.Equals(previousBlock.Hash);
+        }
+
+        public static bool BlocksAreNotNull(Block firstBlock, Block secondBlock)
+        {
+            return firstBlock != null && secondBlock != null;
+        }
+    }
+}
