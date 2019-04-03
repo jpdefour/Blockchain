01fcf878cf2c311e86a14c22df6b182b229136c4
John <johnd4390@gmail.com>
2019-04-02 20:48:33 -0400

    added better console logging for clarity

--- BlockChain/Program.cs
+++ BlockChain/Program.cs
@@ -10,20 +10,28 @@ namespace BlockChain
 {
     class Program
     {
-        const int Difficulty = 3;
+        const int Difficulty = 6;
 
         static void Main(string[] args)
         {
+            Console.WriteLine("Creating blockchain with genesis block");
             Blockchain blockchain = new Blockchain(Difficulty);
+            Console.WriteLine("Created blockchain with genesis block");
 
-            blockchain.AddBlock(blockchain.NewBlock("Cucumber"));
-            blockchain.AddBlock(blockchain.NewBlock("Apple"));
-            blockchain.AddBlock(blockchain.NewBlock("Banana"));
+            CreateBlockInBlockchain(blockchain, "Cucumber");
+            CreateBlockInBlockchain(blockchain, "Apple");
+            CreateBlockInBlockchain(blockchain, "Banana");
 
             Console.WriteLine("Blockchain valid? " + blockchain.IsValid());
             Console.WriteLine(blockchain);
             Console.ReadLine();
+        }
 
+        private static void CreateBlockInBlockchain(Blockchain blockchain, string data)
+        {
+            Console.WriteLine("Creating block with data: " + data);
+            blockchain.AddBlock(blockchain.NewBlock(data));
+            Console.WriteLine("Created block with data: " + data);
         }
     }
 }
